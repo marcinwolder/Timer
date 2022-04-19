a7e4b40c9e16b69762060e349be16f855e18a57b
Folderas <82723806+marcinwolder@users.noreply.github.com>
2022-04-19 22:55:57 +0200

    1

--- js.js
+++ js.js
@@ -3,19 +3,30 @@ const playBtn = document.querySelector('#playBtn');
 const pauseBtn = document.querySelector('#pauseBtn');
 const circle = document.querySelector('circle');
 
-const radius = circle.getAttribute('r');
+const timerBox = timeElement.parentElement.parentElement;
+const timerBoxBound = timerBox.getBoundingClientRect();
+
+const radius = (9 * timerBoxBound.width) / 20;
 const circumference = 2 * Math.PI * radius;
+
+circle.setAttribute('cx', `${timerBoxBound.width / 2}px`);
+circle.setAttribute('cy', `${timerBoxBound.height / 2}px`);
+circle.setAttribute('stroke-width', `${timerBoxBound.width / 20}px`);
+circle.setAttribute('r', `${radius}px`);
+
+console.log(timerBoxBound);
 
 time.value = '3:00';
 let startingTime;
 
 const timer = new Timer(timeElement, playBtn, pauseBtn, {
   onStart(duration) {
+    circle.style.visibility = 'visible';
     circle.setAttribute('stroke-dasharray', `${circumference}px`);
     startingTime = duration;
   },
   onTick(currentTime) {
-    const x = circumference * (1 - currentTime / startingTime);
-    circle.setAttribute('stroke-dashoffset', `-${x}px`);
+    const x = circumference * (currentTime / startingTime);
+    circle.setAttribute('stroke-dashoffset', `${x}px`);
   },
 });
